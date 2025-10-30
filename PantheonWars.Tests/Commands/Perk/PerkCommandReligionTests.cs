d13370c0f2d869baf3558066a416407cbc863f55
quantumheart <cody.mumford9106@hotmail.com>
2025-10-30 10:34:45 -0500

    feat: add OnPerksReligion_ReligionHasNoUnlockedPerks_ReturnsInfoMessage

--- PantheonWars.Tests/Commands/Perk/PerkCommandReligionTests.cs
+++ PantheonWars.Tests/Commands/Perk/PerkCommandReligionTests.cs
@@ -56,4 +56,40 @@ public class PerkCommandReligionTests : PerkCommandsTestHelpers
         // Assert
         Assert.Equal(ErrorMessageConstants.ErrorNoReligion, result.StatusMessage);
     }
+
+    [Fact]
+    public void OnPerksReligion_ReligionHasNoUnlockedPerks_ReturnsInfoMessage()
+    {
+        // Arrange
+        var player = new Mock<IServerPlayer>().Object;
+        var playerData = new PlayerReligionData()
+        {
+            ReligionUID = "foobar"
+        };
+        var religion = new ReligionData()
+        {
+            ReligionUID = "foobar"
+        };
+
+        var args = new TextCommandCallingArgs
+        {
+            Caller = new Caller
+            {
+                Player = player
+            }
+        };
+
+        _playerReligionDataManager.Setup(p => p.GetOrCreatePlayerData(player.PlayerUID))
+            .Returns(playerData);
+        _religionManager.Setup(r => r.GetReligion(It.IsAny<string>()))
+            .Returns(religion);
+        _perkEffectSystem.Setup(p => p.GetActivePerks(player.PlayerUID))
+            .Returns((new List<Models.Perk>(), new List<Models.Perk>()));
+
+        // Act
+        var result = _sut!.OnPerksReligion(args);
+
+        // Assert
+        Assert.Equal(InfoMessageConstants.InfoNoReligionPerks, result.StatusMessage);
+    }
 }
