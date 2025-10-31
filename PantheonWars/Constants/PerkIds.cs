5124f1f1dc20b78be606439d05ffbdb150abec9e
quantumheart <cody.mumford9106@hotmail.com>
2025-10-31 09:06:26 -0500

    feat: add constant ids

--- PantheonWars/Constants/PerkIds.cs
+++ PantheonWars/Constants/PerkIds.cs
@@ -60,33 +60,93 @@ namespace PantheonWars.Constants
 
         #endregion
 
-        #region Aethra (Light) - Placeholder
+        #region Aethra (Light) - 10 Perks
 
-        // TODO: Add Aethra perk IDs when perks are designed
+        // Player Perks (6)
+        public const string AethraDivineGrace = "aethra_divine_grace";
+        public const string AethraRadiantStrike = "aethra_radiant_strike";
+        public const string AethraBlessedShield = "aethra_blessed_shield";
+        public const string AethraPurifyingLight = "aethra_purifying_light";
+        public const string AethraAegisOfLight = "aethra_aegis_of_light";
+        public const string AethraAvatarOfLight = "aethra_avatar_of_light";
 
-        #endregion
-
-        #region Umbros (Shadows) - Placeholder
-
-        // TODO: Add Umbros perk IDs when perks are designed
-
-        #endregion
-
-        #region Tharos (Storms) - Placeholder
-
-        // TODO: Add Tharos perk IDs when perks are designed
-
-        #endregion
-
-        #region Gaia (Earth) - Placeholder
-
-        // TODO: Add Gaia perk IDs when perks are designed
+        // Religion Perks (4)
+        public const string AethraBlessingOfLight = "aethra_blessing_of_light";
+        public const string AethraDivineSanctuary = "aethra_divine_sanctuary";
+        public const string AethraSacredBond = "aethra_sacred_bond";
+        public const string AethraCathedralOfLight = "aethra_cathedral_of_light";
 
         #endregion
 
-        #region Vex (Madness) - Placeholder
+        #region Umbros (Shadows) - 10 Perks
 
-        // TODO: Add Vex perk IDs when perks are designed
+        // Player Perks (6)
+        public const string UmbrosShadowBlend = "umbros_shadow_blend";
+        public const string UmbrosAssassinate = "umbros_assassinate";
+        public const string UmbrosPhantomDodge = "umbros_phantom_dodge";
+        public const string UmbrosDeadlyAmbush = "umbros_deadly_ambush";
+        public const string UmbrosVanish = "umbros_vanish";
+        public const string UmbrosAvatarOfShadows = "umbros_avatar_of_shadows";
+
+        // Religion Perks (4)
+        public const string UmbrosShadowCult = "umbros_shadow_cult";
+        public const string UmbrosCloak = "umbros_cloak";
+        public const string UmbrosNightAssassins = "umbros_night_assassins";
+        public const string UmbrosEternalDarkness = "umbros_eternal_darkness";
+
+        #endregion
+
+        #region Tharos (Storms) - 10 Perks
+
+        // Player Perks (6)
+        public const string TharosStormborn = "tharos_stormborn";
+        public const string TharosLightningStrike = "tharos_lightning_strike";
+        public const string TharosStormRider = "tharos_storm_rider";
+        public const string TharosThunderlord = "tharos_thunderlord";
+        public const string TharosTempest = "tharos_tempest";
+        public const string TharosAvatarOfStorms = "tharos_avatar_of_storms";
+
+        // Religion Perks (4)
+        public const string TharosStormCallers = "tharos_storm_callers";
+        public const string TharosLightningChain = "tharos_lightning_chain";
+        public const string TharosThunderstorm = "tharos_thunderstorm";
+        public const string TharosEyeOfTheStorm = "tharos_eye_of_the_storm";
+
+        #endregion
+
+        #region Gaia (Earth) - 10 Perks
+
+        // Player Perks (6)
+        public const string GaiaEarthenResilience = "gaia_earthen_resilience";
+        public const string GaiaStoneForm = "gaia_stone_form";
+        public const string GaiaNaturesBlessing = "gaia_natures_blessing";
+        public const string GaiaMountainGuard = "gaia_mountain_guard";
+        public const string GaiaLifebloom = "gaia_lifebloom";
+        public const string GaiaAvatarOfEarth = "gaia_avatar_of_earth";
+
+        // Religion Perks (4)
+        public const string GaiaEarthwardens = "gaia_earthwardens";
+        public const string GaiaLivingFortress = "gaia_living_fortress";
+        public const string GaiaNaturesWrath = "gaia_natures_wrath";
+        public const string GaiaWorldTree = "gaia_world_tree";
+
+        #endregion
+
+        #region Vex (Madness) - 10 Perks
+
+        // Player Perks (6)
+        public const string VexMaddeningWhispers = "vex_maddening_whispers";
+        public const string VexChaoticFury = "vex_chaotic_fury";
+        public const string VexDeliriumShield = "vex_delirium_shield";
+        public const string VexPandemonium = "vex_pandemonium";
+        public const string VexMindFortress = "vex_mind_fortress";
+        public const string VexAvatarOfMadness = "vex_avatar_of_madness";
+
+        // Religion Perks (4)
+        public const string VexCultOfChaos = "vex_cult_of_chaos";
+        public const string VexSharedMadness = "vex_shared_madness";
+        public const string VexInsanityAura = "vex_insanity_aura";
+        public const string VexRealmOfMadness = "vex_realm_of_madness";
 
         #endregion
     }
